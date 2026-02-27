14647f759efbdfff7a298d28203d22a0923de64b
AnushkaChoudhari <anushkachoudhari5@gmail.com>
2026-02-28 03:15:29 +0530

    google maps upadated ui

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const path = require('path');
 
 // Models
